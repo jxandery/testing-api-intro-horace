c23c84a09b8a62ee050a151f4082773e466d85e5
Jack Yeh <jack.yeh@gmail.com>
2015-07-22 10:07:53 -0600

    Stub out the expected return value: - @controller is available in the controller_test automatically through Rails
    - pseudocode:
      # call #user on #twitter_client of the controller
      # it returns some kind of “User” object
      # that object has a method “#followers_count which returns a number
    
    - Create a Jeff object with a :followers_count that returns 1
    - Notice the time difference between our un-stubbed and stubbed versions:
      + unstubbed: Finished in 1.132931s, 0.8827 runs/s, 2.6480 assertions/s.
      + stubbed: Finished in 0.088945s, 11.2429 runs/s, 44.9716 assertions/s.

--- test/controllers/tweet_streams_controller_test.rb
+++ test/controllers/tweet_streams_controller_test.rb
@@ -3,9 +3,14 @@ require 'test_helper'
 class TweetStreamsControllerTest < ActionController::TestCase
 
   test "fetches tweets on create" do
+    jeff = Object.new
+    jeff.expects(:followers_count).returns(1)
+    @controller.twitter_client.expects(:user).with('j3').returns(jeff)
+
     post :create, :twitter_handle => "j3"
     assert_response :success
     assert_not_nil assigns(:tweets)
     assert_select "li.tweet"
   end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/pride'
-require 'mocha/minitest'
+require 'mocha/mini_test'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
